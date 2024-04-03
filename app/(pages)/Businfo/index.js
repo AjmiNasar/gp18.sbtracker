5556ba8c78b10b03465a9dd2f4f46c9347fa7b63
Ajmi <114758659+AjmiNasar@users.noreply.github.com>
2024-04-03 12:47:50 +0530

    Update index.js

--- app/(pages)/Businfo/index.js
+++ app/(pages)/Businfo/index.js
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import Timeline from "react-native-timeline-flatlist";
 
-const index = () => {
+
+const index = (navigation) => {
   var place = "";
-  var time = "";
+  var time;
+  var stop_name = "School";
+  const data = [
+    { time: "09:00", title: "School", delay: "09:00" },
+    { time: "09:10", title: "Pallimoola", delay: "09:15" },
+    { time: "09:17", title: "Cheroor", delay: "09:23" }, // No delay
+    { time: "09:28", title: "asdgfd", delay: "09:28" },
+    { time: "09:35", title: "zfgxhjg", delay: "09:34" }, // No delay
+  ];
+
   return (
     <>
       <View
@@ -19,7 +32,9 @@ const index = () => {
           borderBottomStartRadius: 30,
         }}
       >
-
+      <View>
+      <Entypo style={{top:30,left:130}} name="back-in-time" size={18} color="red" />
+      </View>
         <View
           style={{
             flexDirection: "column",
@@ -34,10 +49,56 @@ const index = () => {
           <Text style={{ color: "black", fontSize: 16 }}>
             Your ward has reached {place}
           </Text>
-          <Text style={{ color: "red", textAlign: "center" }}>will be arriving at{time}
+          <Text style={{ color: "red", textAlign: "center" }}>
+            will be arriving at {time}
           </Text>
-          <Entypo style={{}} name="back-in-time" size={22} color="red" />
         </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: "white",
+          gap: 8,
+          height: 35,
+          justifyContent: "center",
+          width: 70,
+          margin: 40,
+          marginLeft: 20,
+
+          shadowColor: "black",
+          shadowOffset: {
+            width: 0,
+            height: 20,
+          },
+          shadowOpacity: 0.75,
+          shadowRadius: 4,
+          elevation: 10,
+        }}
+      >
+        <Text style={{ color: "red" }}>Bus No 1</Text>
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.busPointer}>
+        <FontAwesome5 name="bus-alt" size={20} color="#4169E1" />
+        </View>
+        <Timeline
+          data={data}
+          circleSize={8}
+          circleColor="#343434"
+          lineColor="black"
+          titleStyle={styles.title}
+          timeContainerStyle={styles.time}
+          descriptionStyle={styles.description}
+          renderDetail={(rowData, sectionID, rowID) => (
+            <View style={styles.detailContainer}>
+              <Text style={styles.title}>{rowData.title}</Text>
+
+              <Text style={styles.description}>{rowData.delay}</Text>
+            </View>
+          )}
+        />
       </View>
     </>
   );
@@ -45,4 +106,31 @@ const index = () => {
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 100,
+    flex: 1,
+    marginTop: 10,
+  },
+  busPointer: {
+    position: "absolute",
+    top: -4,
+    left: 55,
+  },
+  time:{
+    fontsize:10,
+    top:-4
+  },
+  title: {
+    marginTop: -15,
+    fontSize: 15,
+    color: "black",
+    marginBottom: 70,
+  },
+  description: {
+    color: "red",
+   left: -75,
+    top:-70,
+    fontSize:12
+  },
+});
